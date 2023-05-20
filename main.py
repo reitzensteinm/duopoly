7646e1be5a281e87d58ea995f694bf39d247d264
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-05-20 17:36:43 +1000

    Add type hints and docstrings
    Prompt: "Add type hints and a one line docstring to each function"

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import openai
 
 
-def gpt_query(message):
+def gpt_query(message: str) -> str:
+    """Queries GPT-4 with a given message."""
     openai.api_key = os.environ["OPENAI_API_KEY"]
 
     completion = openai.ChatCompletion.create(model="gpt-4", messages=[
@@ -16,16 +17,20 @@ def gpt_query(message):
     return completion.choices[0].message.content
 
 
-def read_file(path):
-    return open(path, 'r', encoding='utf-8').read()
+def read_file(path: str) -> str:
+    """Reads and returns the content of a file."""
+    with open(path, 'r', encoding='utf-8') as file:
+        return file.read()
 
 
-def write_file(path, contents):
+def write_file(path: str, contents: str) -> None:
+    """Writes the contents to a file."""
     with open(path, 'w', encoding='utf-8') as file:
         file.write(contents)
 
 
-def main():
+def main() -> None:
+    """Main function to handle program execution."""
     print("What change would you like to make?")
     prompt = input()
 
