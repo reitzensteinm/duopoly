02539d128b1437232e877eff5d5e378b69ed4c57
reitzensteinm <reitzensteinm@gmail.com>
2023-05-30 22:38:35 +1000

    Add move code recipe

--- recipes.py
+++ recipes.py
@@ -4,7 +4,7 @@ from typing import List
 
 @dataclass
 class Recipe:
-    id: int
+    id: str
     description: str
     prompt: str
 
@@ -12,6 +12,16 @@ class Recipe:
 recipes_list: List[Recipe] = []
 
 
-def create_recipe(id: int, description: str, prompt: str):
+def create_recipe(id: str, description: str, prompt: str):
     recipe = Recipe(id, description, prompt)
     recipes_list.append(recipe)
+
+
+MOVE = """To move code from one file to another, the following steps must be taken: 
+ * Delete the code in the source file
+ * Add the code in the destination file 
+ * Delete any unused imports in the source file
+ * Add any imports required in the destination file
+ * Add an import to the source file to be able to call the code in the destination file"""
+
+create_recipe("MOVE", "Move code between files", MOVE)
