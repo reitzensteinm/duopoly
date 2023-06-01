f7ed6782928fb11e94251db138a26b523612fb3f
reitzensteinm <reitzensteinm@gmail.com>
2023-06-01 21:50:49 +1000

    Force push to origin

--- repo.py
+++ repo.py
@@ -17,7 +17,7 @@ def switch_and_reset_branch(branch_id: str):
 def push_local_branch_to_origin(branch_id: str):
     repo = Repo(os.getcwd())
     repo.git.checkout(branch_id)
-    repo.git.push("origin", branch_id)
+    repo.git.push("origin", branch_id, force=True)
 
 
 def create_pull_request(repo_name: str, branch_id: str, title: str, body: str):
