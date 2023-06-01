c6b2f0f5d77d7e1f4bfd9d1f8639c86fddb8eb16
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-01 22:35:00 +1000

    Local modifications committed

--- main.py
+++ main.py
@@ -123,9 +123,8 @@ def process_issue(issue: Issue) -> None:
     for k, v in updated_files.items():
         write_file(k, v)
 
-    repo.commit_local_modifications()
+    repo.commit_local_modifications(issue.title, issue.description)
     repo.push_local_branch_to_origin(branch_id)
-
     if not repo.check_pull_request_title_exists("reitzensteinm/duopoly", issue.title):
         repo.create_pull_request(
             repo_name="reitzensteinm/duopoly",
