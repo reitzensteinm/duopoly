e790076b8cc45b6838ef29e7d77e53e7de4bd65f
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-05 23:19:15 +1000

    Make partition tests more succinct
    Prompt: "The partition_by_predicate tests are good, but much too long. Make them neater, and use pytest features like parameterization if necessary."

--- test_utils.py
+++ test_utils.py
@@ -2,36 +2,33 @@ import pytest
 from utils import partition_by_predicate
 
 
-def test_partition_by_predicate_even():
-    assert partition_by_predicate([1, 2, 3, 4, 5, 6, 8, 10], lambda x: x % 2 == 0) == [
-        [1],
-        [2, 3],
-        [4, 5],
-        [6],
-        [8],
-        [10],
-    ]
+import pytest
+from utils import partition_by_predicate
 
 
-def test_partition_by_predicate_empty_list():
-    assert partition_by_predicate([], lambda x: x % 2 == 0) == []
-
-
-def test_partition_by_predicate_no_true_predicate():
-    assert partition_by_predicate([1, 3, 5, 7, 9], lambda x: x % 2 == 0) == [
-        [1, 3, 5, 7, 9]
-    ]
-
-
-def test_partition_by_predicate_all_true_predicate():
-    assert partition_by_predicate([2, 4, 6, 8, 10], lambda x: x % 2 == 0) == [
-        [2],
-        [4],
-        [6],
-        [8],
-        [10],
-    ]
-
-
-def test_partition_by_predicate_single_element():
-    assert partition_by_predicate([3], lambda x: x % 2 == 0) == [[3]]
+@pytest.mark.parametrize(
+    "input_list, expected",
+    [
+        (
+            [1, 2, 3, 4, 5, 6, 8, 10],
+            [[1], [2, 3], [4, 5], [6], [8], [10]],
+        ),
+        (
+            [],
+            [],
+        ),
+        (
+            [1, 3, 5, 7, 9],
+            [[1, 3, 5, 7, 9]],
+        ),
+        (
+            [2, 4, 6, 8, 10],
+            [[2], [4], [6], [8], [10]],
+        ),
+    ],
+)
+def test_partition_by_predicate(
+    input_list,
+    expected,
+):
+    assert partition_by_predicate(input_list, lambda x: x % 2 == 0) == expected
