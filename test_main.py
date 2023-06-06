b8f31344c604b3e7be24771fc66da80b0fdc7874
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-06 21:59:32 +1000

    Add test to apply_prompt_to_files
    Prompt: "Add a test for the main.py apply_prompt_to_files function in test_main.py. The prompt should be "create a function called add_test which adds one to the input", with an empty file for input. Take the resulting output code, and exec it, extracting the add_test function from the resulting environment. Test whether the add_test function works."

---
+++ test_main.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+
+from main import apply_prompt_to_files
+
+
+@pytest.fixture
+def input_file():
+    empty_file = ""
+    return empty_file
+
+
+def test_apply_prompt_to_files(input_file):
+    prompt = "create a function called add_test which adds one to the input"
+    result_files = apply_prompt_to_files(prompt, {"test_file.py": input_file})
+    test_file = result_files["test_file.py"]
+    exec_globals = {}
+    exec(test_file, exec_globals)
+    add_test = exec_globals["add_test"]
+    assert add_test(3) == 4
+    assert add_test(-5) == -4
+    assert add_test(0) == 1
