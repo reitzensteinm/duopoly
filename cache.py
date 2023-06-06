94319a4da509fad8d8415dc15ca292e2e0ea9e5a
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-06 22:23:12 +1000

    Add caching system
    Prompt: "Add a cache.py file, which implements a key value store on the disk with read and write. Values should be stored in a .cache/ directory, using md5 hash of the keys. Reading a non existent key should throw an exception.
    
    Add a function which can memoize function calls by storing their arguments to the KV store."

---
+++ cache.py
@@ -0,0 +1,47 @@
+import os
+import hashlib
+
+
+class KeyValueStore:
+    def __init__(self):
+        self.cache_dir = ".cache/"
+        if not os.path.exists(self.cache_dir):
+            os.makedirs(self.cache_dir)
+
+    def read(self, key):
+        key_hash = self._hash_key(key)
+        file_path = self.cache_dir + key_hash
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"Key not found: {key}")
+
+        with open(file_path, "r") as file:
+            value = file.read()
+        return value
+
+    def write(self, key, value):
+        key_hash = self._hash_key(key)
+        file_path = self.cache_dir + key_hash
+
+        with open(file_path, "w") as file:
+            file.write(value)
+
+    def _hash_key(self, key):
+        md5_hash = hashlib.md5()
+        md5_hash.update(key.encode("utf-8"))
+        hashed_key = md5_hash.hexdigest()
+        return hashed_key
+
+
+def memoize(func):
+    kv_store = KeyValueStore()
+
+    def memoized_func(*args, **kwargs):
+        key = f"{func.__name__}_{str(args)}_{str(kwargs)}"
+        try:
+            return kv_store.read(key)
+        except FileNotFoundError:
+            result = func(*args, **kwargs)
+            kv_store.write(key, result)
+            return result
+
+    return memoized_func
