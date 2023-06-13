80c7970a26eb16de25dffc4bdb9b2c849b9d9999
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-13 21:25:22 +1000

    Create tests for synchronized
    Prompt: "Create a test for synchronized in utils/threading.py. Call a function twice, using time.sleep(), and ensure that the bodies of the function do not ever execute at the same time.
    
    Create another test that tests the re-enterant behaviour of synchronised, performing a small amount of recursion to ensure that a synchronized function can call itself."

--- utils/threading.py
+++ utils/threading.py
@@ -1,4 +1,6 @@
 import threading
+import unittest
+import time
 
 _lock = threading.RLock()
 
@@ -9,3 +11,44 @@ def synchronized(func):
             return func(*args, **kwargs)
 
     return _synchronized
+
+
+class TestSynchronized(unittest.TestCase):
+    def test_non_reentrant_synchronized(self):
+        execution_counter = 0
+
+        @synchronized
+        def synchronized_function():
+            nonlocal execution_counter
+            execution_counter += 1
+            time.sleep(1)
+            execution_counter -= 1
+
+        t1 = threading.Thread(target=synchronized_function)
+        t2 = threading.Thread(target=synchronized_function)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
+        self.assertEqual(
+            execution_counter, 0, "Synchronized function executed concurrently"
+        )
+
+    def test_reentrant_synchronized(self):
+        call_counter = 0
+
+        @synchronized
+        def reentrant_synchronized_function(level):
+            nonlocal call_counter
+            call_counter += 1
+            if level > 1:
+                reentrant_synchronized_function(level - 1)
+            call_counter -= 1
+
+        reentrant_synchronized_function(3)
+        self.assertEqual(call_counter, 0, "Synchronized reentrant function failed")
+
+
+if __name__ == "__main__":
+    unittest.main()
