784229fb48b4e0f78b27ecc9da92ab0b85f38581
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-18 23:14:05 +1000

    Load prompts YAML in evals
    Prompt: "In process_evals, use PyYAML to load the prompts.yaml file in the directory passed in.
    
    Create an Eval dataclass, which contains a prompt string, and a tests string array. Read an array of Evals from the prompts.yaml file.
    
    This is an example of the file format:
    
    ```basic:
      prompt: "In main.py, rename the main function to main2"
      tests: [ "test_main_rename" ]
    ambiguous_file:
      prompt: "Rename the main function to main2"
      tests: [ "test_main_rename" ]
    short:
      prompt: "Rename the main to main2"
      tests: [ "test_main_rename" ]```"

--- src/evals/evals.py
+++ src/evals/evals.py
@@ -1,2 +1,15 @@
+from dataclasses import dataclass
+from typing import List
+import yaml
+
+
+@dataclass
+class Eval:
+    prompt: str
+    tests: List[str]
+
+
 def process_evals(directory: str):
-    pass
+    with open(f"{directory}/prompts.yaml", "r") as file:
+        data = yaml.safe_load(file)
+        evals = [Eval(prompt=key, tests=value["tests"]) for key, value in data.items()]
