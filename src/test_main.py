2637d8783571368d6633ba8aaa8193286471597f
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-18 23:17:11 +1000

    Add src to test_main
    Prompt: "In test_main, the test_file.py path should be modified to src/test_file.py"

--- src/test_main.py
+++ src/test_main.py
@@ -12,8 +12,8 @@ def input_file():
 
 def test_apply_prompt_to_files(input_file):
     prompt = "create a function called add_test which adds one to the input"
-    result_files = apply_prompt_to_files(prompt, {"test_file.py": input_file})
-    test_file = result_files["test_file.py"]
+    result_files = apply_prompt_to_files(prompt, {"src/test_file.py": input_file})
+    test_file = result_files["src/test_file.py"]
     exec_globals = {}
     exec(test_file, exec_globals)
     add_test = exec_globals["add_test"]
