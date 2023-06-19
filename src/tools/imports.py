d1b21a058620abd3989a1530dc50f72dc17d3ee9
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-20 00:17:55 +1000

    Use model constants in tools
    Prompt: "In the tools files which call GPT, use the new model constants in gpt.py instead of naming the model string directly."

--- src/tools/imports.py
+++ src/tools/imports.py
@@ -1,4 +1,4 @@
-from gpt import gpt_query
+from gpt import gpt_query, GPT_3_5
 
 SYSTEM_IMPORTS = """
 Extract any imports from the following source code, returning only the lines that import other files.
@@ -6,5 +6,5 @@ Extract any imports from the following source code, returning only the lines tha
 
 
 def imports(source_code: str) -> str:
-    result = gpt_query(source_code, system=SYSTEM_IMPORTS, model="gpt-3.5-turbo")
+    result = gpt_query(source_code, system=SYSTEM_IMPORTS, model=GPT_3_5)
     return result
