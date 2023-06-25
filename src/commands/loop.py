520eed097780471f9153c56bbd1250322ad89bb4
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-25 23:26:13 +1000

    Use Command State in Command Loop
    Prompt: "In commands/loop.py, in the command loop, create a command state (from commands/state.py) at the start of the loop.
    
    Instead of adding output to the prompt, apply it to the State's scratch property, and append the scratch property to prompt when passing it into gpt_query.
    
    Add files as an optional param to command_loop, which is a dict of strings to strings, defaulting to empty dict. Pass it into the constructor for State."

--- src/commands/loop.py
+++ src/commands/loop.py
@@ -1,15 +1,17 @@
 import json
 from commands.command import Command, parse_gpt_response
 from gpt import gpt_query
+from commands.state import State
 
 
-def command_loop(prompt: str, system: str, command_classes: list):
+def command_loop(prompt: str, system: str, command_classes: list, files: dict = {}):
+    state = State(files)
     while True:
-        result = gpt_query(prompt, system, command_classes)
+        result = gpt_query(prompt + "\n" + state.scratch, system, command_classes)
         command = parse_gpt_response(command_classes, json.loads(result))
 
         if command.terminal:
             return command
 
         output = command.execute()
-        prompt += "\n" + output
+        state.scratch += "\n" + output
