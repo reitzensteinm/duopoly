30bb1cd2c2cc21c8a802feb34663c6b117a2a484
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-06-27 23:23:32 +1000

    Call extract_schemas in the command loop
    Prompt: "In the command loop in loop.py, call extract_schemas from commands/command.py on the commands that are passed to the function before passing them through to gpt_query."

--- src/commands/loop.py
+++ src/commands/loop.py
@@ -1,11 +1,12 @@
 import json
-from commands.command import Command, parse_gpt_response
+from commands.command import Command, parse_gpt_response, extract_schemas
 from gpt import gpt_query
 from commands.state import State
 
 
 def command_loop(prompt: str, system: str, command_classes: list, files: dict = {}):
     state = State(files)
+    command_classes = extract_schemas(command_classes)
     while True:
         result = gpt_query(prompt + "\n" + state.scratch, system, command_classes)
         command = parse_gpt_response(command_classes, json.loads(result))
