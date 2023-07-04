6763e3462b8b9195e120ad76dd07e846d390df74
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-05 00:00:33 +1000

    Fix State init bug
    Prompt: "In state.py, the state files and original files fields are being initialized incorrectly."

--- src/commands/state.py
+++ src/commands/state.py
@@ -1,8 +1,9 @@
 from typing import Dict
+import copy
 
 
 class State:
     def __init__(self, files_dict: Dict[str, str]):
-        self.files: Dict[str, str] = dict(files_dict)
-        self.original_files: Dict[str, str] = dict(files_dict)
+        self.files: Dict[str, str] = copy.deepcopy(files_dict)
+        self.original_files: Dict[str, str] = copy.deepcopy(files_dict)
         self.scratch: str = ""
