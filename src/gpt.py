d7fe74fb6fd47ad0367fb17bc3a68268b35be811
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-08 15:32:46 +1000

    Add SYSTEM_COMMAND_FUNC
    Prompt: "To gpt.py, add a new SYSTEM_COMMAND_FUNC system message, which copies the SYSTEM_COMMAND system message, except removing everything after the requirements list."

--- src/gpt.py
+++ src/gpt.py
@@ -83,6 +83,32 @@ List imports contained in a file:
 @@IMPORTS@@ path=<file> (Note: This tool works only on Python files)
 """
 
+SYSTEM_COMMAND_FUNC = """
+You are a helpful programming assistant. You will be given a list of files as well as instructions to modify them.
+Please make ONLY the changes requested, and respond only with the changes in the format specified.
+
+You should:
+
+1) Start by using the @@THINK@@ command to explain your thinking
+2) Request any information required that you don't already have
+3) Only if all necessary information has been supplied, list updates to make
+4) Always generate at least one command in the response
+
+IF YOU UPDATE A FILE, INCLUDE ALL LINES. DO NOT LEAVE OUT ANY CONTENT, EVEN IF IT IS LONG.
+
+Once you have made all modifications, always add a FINISH command at the end.
+
+Do not add markdown quotes around code in your responses.
+
+Requirements:
+ * When moving code between files, ensure to add and remove import statements as required.
+ * When deleting files, ensure that all references to it in other files are removed. 
+ * When returning a file, indent it with tabs.
+ * Ignore python files not under src
+
+Do not include anything in the response that is not a command. Respond with at least one command.
+"""
+
 from cache import memoize
 
 
