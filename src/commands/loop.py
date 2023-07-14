b9c36a8fc8193d8db29d3a8c45dbc306495a1952
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-14 19:08:40 +1000

    Add retries to command_loop_new
    Prompt: "In loop.py, if an exception is thrown during the loop, simply try again, up to a maximum of 5 times."

--- src/commands/loop.py
+++ src/commands/loop.py
@@ -28,18 +28,27 @@ def stringify_command(command: Command) -> str:
 
 def command_loop_new(prompt: str, system: str, command_classes: list, files: dict = {}):
     state = State(files)
+    exception_count = 0
 
     while True:
-        result = gpt_query(
-            prompt + "\n" + state.scratch, system, extract_schemas(command_classes)
-        )
-        command = parse_gpt_response(command_classes, result)
+        try:
+            result = gpt_query(
+                prompt + "\n" + state.scratch, system, extract_schemas(command_classes)
+            )
+            command = parse_gpt_response(command_classes, result)
 
-        if command.terminal:
-            return (command, state)
+            if command.terminal:
+                return (command, state)
 
-        # Adding stringified command to scratch
-        state.scratch += "\n" + stringify_command(command)
+            # Adding stringified command to scratch
+            state.scratch += "\n" + stringify_command(command)
 
-        output = command.execute(state)
-        state.scratch += "\n" + output
+            output = command.execute(state)
+            state.scratch += "\n" + output
+
+            # If command execute successfully, reset the exception counter
+            exception_count = 0
+        except Exception:
+            exception_count += 1
+            if exception_count >= 5:
+                break
