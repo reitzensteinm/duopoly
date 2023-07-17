2931f20bd642913d4c03394f52152f9a501e3a90
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-17 19:12:40 +1000

    Move Verdict
    Prompt: "Create a command_verdict.py file in the commands directory. Copy the Verdict command from commands/command.py over. Don't modify any other files yet.
    
    Import Command from commands/command.py and State from commands/state.py"

---
+++ src/commands/command_verdict.py
@@ -0,0 +1,57 @@
+from commands.command import Command
+from commands.state import State
+
+
+class Verdict(Command):
+    """
+    Class representing a Verdict command.
+    """
+
+    @classmethod
+    def name(cls) -> str:
+        return "Verdict"
+
+    @property
+    def terminal(self):
+        return True
+
+    def __init__(self, reasoning: str, verdict: bool):
+        self.reasoning: str = reasoning
+        self.verdict: bool = verdict
+
+    @staticmethod
+    def schema() -> dict:
+        """
+        Returns the schema for the Verdict command.
+        """
+        return {
+            "name": "Verdict",
+            "description": "Make a decision on whether the proposed change is acceptable or not",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "reasoning": {
+                        "type": "string",
+                        "description": "Text that describes the reason for the supplied verdict",
+                    },
+                    "verdict": {
+                        "type": "boolean",
+                        "description": "A Boolean that describes whether or not the change is judged to be successful",
+                    },
+                },
+                "required": ["reasoning", "verdict"],
+            },
+        }
+
+    @staticmethod
+    def load_from_json(json_data: dict) -> "Verdict":
+        """
+        Loads the Verdict command from the provided json_data.
+        """
+        return Verdict(json_data["reasoning"], json_data["verdict"])
+
+    def execute(self, state: State) -> tuple:
+        """
+        Executes the Verdict command.
+        """
+        return self.reasoning, self.verdict
