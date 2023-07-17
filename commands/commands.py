bbf90dfb1d7d5d72940ce07f73c5af8894edbee8
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-17 16:21:16 +1000

    Create Commands
    Prompt: "Create commands/commands.py, which exists to re-export the Command classes in commands/command.py, which will later be moved around.
    
    I should be able to do e.g. from commands.commands import Think"

---
+++ commands/commands.py
@@ -0,0 +1,9 @@
+# commands/commands.py
+
+from .command import Command
+from .command import Think
+from .command import Verdict
+from .command import Files
+from .command import ReplaceFile
+from .command import Search
+from .command import DeleteFile
