ebb2c6b179445a65eb81a184a4aff6884535fb98
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-17 15:48:14 +1000

    Copy utility functions to loop.py
    Prompt: "Copy extract_schemas and parse_gpt_response from commands/command.py to commands/loop.py, and remove their imports in commands/loop.py. Don't modify commands/command.py."

--- src/commands/loop.py
+++ src/commands/loop.py
@@ -1,21 +1,38 @@
 import json
-from commands.command import Command, parse_gpt_response, extract_schemas
+from commands.command import Command
 from gpt import gpt_query
 from commands.state import State
 from termcolor import cprint
+
+
+def extract_schemas(command_classes):
+    """
+    Extracts schemas from a list of command classes.
+    """
+    schemas = [command_class.schema() for command_class in command_classes]
+    return schemas
+
+
+def parse_gpt_response(command_classes, gpt_response):
+    """
+    Parses the response from GPT and returns a Command instance for that response.
+    """
+    for command_class in command_classes:
+        if command_class.name() == gpt_response["name"]:
+            return command_class.load_from_json(json.loads(gpt_response["arguments"]))
+
+    raise ValueError(f"Unrecognized command name: {gpt_response['name']}")
 
 
 def stringify_command(command: Command) -> str:
     """Converts the command into a string representation."""
     parameters_string = ""
 
-    # Handling specific command types
     if type(command).__name__ == "Think":
         parameters_string = f"thought={command.thought}"
     elif type(command).__name__ == "Files":
         parameters_string = f"files={command.files}"
     elif type(command).__name__ == "ReplaceFile":
-        # Include actual 'filename' and 'content' from the 'ReplaceFile' command
         parameters_string = (
             f"filename={command.filename}, instructions={command.instructions}"
         )
@@ -45,12 +62,10 @@ def command_loop_new(prompt: str, system: str, command_classes: list, files: dic
 
             output = command.execute(state)
 
-            # Adding stringified command to scratch
             state.scratch += "\n" + stringify_command(command)
 
             state.scratch += "\n" + output
 
-            # If command execute successfully, reset the exception counter
             exception_count = 0
         except Exception as e:
             exception_count += 1
