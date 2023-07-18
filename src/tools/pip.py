8ddb60d77c26e6ebef8fd5f28788180567e142cc
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-18 20:44:51 +1000

    Add tool to add package
    Prompt: "Create a new file tools/pip.py, which contains a function to add the specified package, passed in as a string.
    
    It should call out to the command line:
    
    pip3 install <package name>
    pip3 freeze > requirements.txt
    
    If this fails, try using pip instead of pip3 because Python is awful."

---
+++ src/tools/pip.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+
+
+def install_package(package_name):
+    try:
+        subprocess.call("pip3 install {}".format(package_name), shell=True)
+    except:
+        print("Failed to install using pip3. Retrying with pip...")
+        try:
+            subprocess.call("pip install {}".format(package_name), shell=True)
+        except Exception as err:
+            print("Failed to install package using pip.")
+            return False
+    try:
+        subprocess.call("pip3 freeze > requirements.txt", shell=True)
+    except:
+        print("Failed to freeze requirements using pip3. Retrying with pip...")
+        try:
+            subprocess.call("pip freeze > requirements.txt", shell=True)
+        except Exception as err:
+            print("Failed to freeze requirements using pip.")
+            return False
+    return True
