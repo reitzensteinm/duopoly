4cd6135a9334dc11f59ed4db00f209fd5f956ece
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-19 21:34:43 +1000

    Add Command to Install Package
    Prompt: "Add an InstallPackage command to commands/command_install_package.py.
    
    Look in commands/replace_file.py for an example of an existing command.
    
    It should take one parameter, which is the name of the tool to install, and it should call tools/pip.py to install the package.
    
    Add the command to COMMANDS_GENERATE in commands/commands.py to make it available."

---
+++ src/commands/command_install_package.py
@@ -0,0 +1,57 @@
+from commands.command import Command
+from commands.state import State
+from tools.pip import install_package
+
+
+class InstallPackage(Command):
+    """
+    Class representing InstallPackage command.
+    """
+
+    @classmethod
+    def name(cls) -> str:
+        return "InstallPackage"
+
+    @property
+    def terminal(self):
+        return False
+
+    def __init__(self, tool: str):
+        self.tool: str = tool
+
+    @staticmethod
+    def schema() -> dict:
+        """
+        Returns the schema for the InstallPackage command.
+        """
+        return {
+            "name": "InstallPackage",
+            "description": "Install a tool using pip",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "tool": {
+                        "type": "string",
+                        "description": "Name of the tool to be installed",
+                    },
+                },
+                "required": ["tool"],
+            },
+        }
+
+    @staticmethod
+    def load_from_json(json_data: dict) -> "InstallPackage":
+        """
+        Loads the InstallPackage command from the provided json_data.
+        """
+        return InstallPackage(json_data["tool"])
+
+    def execute(self, state: State):
+        """
+        Executes the InstallPackage command.
+        """
+        install_package(self.tool)
+        return f"Tool {self.tool} has been installed."
+
+    def __str__(self):
+        return f"Function Called: {self.name()} tool={self.tool}"
