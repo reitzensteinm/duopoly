22ea290549903715d9654a480e7ef260ac4ca1e7
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-19 21:34:43 +1000

    Add Command to Install Package
    Prompt: "Add an InstallPackage command to commands/command_install_package.py.
    
    Look in commands/replace_file.py for an example of an existing command.
    
    It should take one parameter, which is the name of the tool to install, and it should call tools/pip.py to install the package.
    
    Add the command to COMMANDS_GENERATE in commands/commands.py to make it available."

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -7,6 +7,15 @@ from .command_files import Files
 from .command_replace_file import ReplaceFile
 from .command_search import Search
 from .command_delete_file import DeleteFile
+from .command_install_package import InstallPackage
 
 COMMANDS_CHECK = [Think, Verdict]
-COMMANDS_GENERATE = [Think, Verdict, Files, ReplaceFile, Search, DeleteFile]
+COMMANDS_GENERATE = [
+    Think,
+    Verdict,
+    Files,
+    ReplaceFile,
+    Search,
+    DeleteFile,
+    InstallPackage,
+]
