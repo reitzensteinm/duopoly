e6cfbde454a32e2ae5254533dfc4d9cfb5881960
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-20 23:52:35 +1000

    Add ReplaceNode to Generate Commands
    Prompt: "Add ReplaceNode to GENERATE_COMMANDS"

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -8,6 +8,7 @@ from .command_replace_file import ReplaceFile
 from .command_search import Search
 from .command_delete_file import DeleteFile
 from .command_install_package import InstallPackage
+from .command_replace_node import ReplaceNode
 
 COMMANDS_CHECK = [Think, Verdict, Files]
 COMMANDS_GENERATE = [
@@ -18,4 +19,5 @@ COMMANDS_GENERATE = [
     Search,
     DeleteFile,
     InstallPackage,
+    ReplaceNode,
 ]
