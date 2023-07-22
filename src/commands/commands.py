baa9cdcff938aea3929de69da5c27b78571f0d07
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-22 14:46:30 +1000

    Remove ReplaceNode
    Prompt: "Remove ReplaceNode from the available commands in commands.py."

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -8,7 +8,6 @@ from .command_replace_file import ReplaceFile
 from .command_search import Search
 from .command_delete_file import DeleteFile
 from .command_install_package import InstallPackage
-from .command_replace_node import ReplaceNode
 
 COMMANDS_CHECK = [Think, Verdict, Files]
 COMMANDS_GENERATE = [
@@ -19,5 +18,4 @@ COMMANDS_GENERATE = [
     Search,
     DeleteFile,
     InstallPackage,
-    ReplaceNode,
 ]
