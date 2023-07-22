0eb53a67a03abad1cc269db7a6e917f5a8c048d3
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-22 20:24:01 +1000

    Load prompts
    Prompt: "In gpt.py, use the load_prompt function defined in prompts.py to load SYSTEM_CHECK_FUNC and SYSTEM_COMMAND_FUNC from check.txt and command.txt respectively."

--- src/gpt.py
+++ src/gpt.py
@@ -6,40 +6,13 @@ from termcolor import cprint
 from tracing.trace import trace
 from tracing.tags import GPT_INPUT, GPT_OUTPUT
 from cache import memoize
+from utilities.prompts import load_prompt
 
 GPT_3_5 = "gpt-3.5-turbo-0613"
 GPT_4 = "gpt-4-0613"
 
-SYSTEM_CHECK_FUNC = """You are a helpful programming assistant. \
-	You will be given original and modified versions of code. \
-	If a file isn't present in the modified version, you can assume it was deleted. \
-	You will also be given a description of the change that was intended. \
-	Was the change that was made correct? \
-	Only respond by calling a function. \
-	Are you absolutely sure? If you have any doubt at all, tell me there is an error. \
-	If a file isn't included in the list, it hasn't been modified, but you can request it with the Files function."""
-
-SYSTEM_COMMAND_FUNC = """
-You are a helpful programming assistant. You will be given a list of files as well as instructions to modify them.
-
-Please make ONLY the changes requested, and respond only with the changes in the format specified.
-
-You should:
-
-1) Start by calling the think function with a step by step description of the changes you're about to make
-2) Request any information required that you don't already have
-3) Only if all necessary information has been supplied, list updates to make
-
-Do not add markdown quotes around code in your responses.
-
-Requirements:
-   * When moving code between files, ensure to add and remove import statements as required.
-   * When deleting files, ensure that all references to it in other files are removed. 
-   * Ignore python files not under src
-   * When returning a file, indent it with tabs.
-
-When finished, call the Verdict function and return to me the results.
-"""
+SYSTEM_CHECK_FUNC = load_prompt("check")
+SYSTEM_COMMAND_FUNC = load_prompt("command")
 
 disable_cache = True
 
@@ -113,9 +86,7 @@ def gpt_query(
     if "function_call" in completion.choices[0].message:
         function_result = completion.choices[0].message["function_call"]
         trace(GPT_OUTPUT, content)
-        cprint(
-            f"Function call result: {function_result}", "cyan"
-        )  # print function call result in cyan
+        cprint(f"Function call result: {function_result}", "cyan")
         return function_result
 
     trace(GPT_OUTPUT, content)
