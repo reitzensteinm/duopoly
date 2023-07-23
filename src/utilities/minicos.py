97fecefb8d6e32f0d714d8207f3867e03c32e5c3
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-23 20:15:05 +1000

    Add a VectorStore class
    Prompt: "Add a VectorStore class to minicos.py, as well as an Entry class.
    
    The Entry class should contain fields:
    
    key, which should be a numpy vector
    value, which should be a string
    
    The VectorStore class should contain a list of Entries, and have a function to add a new entry, with a numpy vector key and string value as parameters.
    
    Specify types within the file, but no enforcement is required."

--- src/utilities/minicos.py
+++ src/utilities/minicos.py
@@ -5,3 +5,17 @@ def cosine_similarity(vec1, vec2):
     vec1_norm = np.linalg.norm(vec1)
     vec2_norm = np.linalg.norm(vec2)
     return np.dot(vec1, vec2) / (vec1_norm * vec2_norm)
+
+
+class Entry:
+    def __init__(self, key: np.ndarray, value: str):
+        self.key = key
+        self.value = value
+
+
+class VectorStore:
+    def __init__(self):
+        self.entries = []
+
+    def add_entry(self, key: np.ndarray, value: str):
+        self.entries.append(Entry(key, value))
