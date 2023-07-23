0a7ac6f2619f8c60a6daab3299efb068098c99b4
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-23 19:19:54 +1000

    Use issue name to create trace
    Prompt: "In main.py, use the issue's name as the name of the trace that's created.
    
    Also, update the add_trace_data calls to use the trace() function from trace.py"

--- src/main.py
+++ src/main.py
@@ -11,38 +11,27 @@ from tracing.tags import EXCEPTION
 import ast
 import astor
 
-MAX_RETRIES = (
-    1  # Maximum number of retries if exception is thrown in process_open_issue
-)
+MAX_RETRIES = 1
 
 
 def merge_approved_prs() -> None:
     approved_prs = repo.find_approved_prs("reitzensteinm/duopoly")
     for pr_id in approved_prs:
-        for attempt in range(
-            5
-        ):  # Attempt to merge the pull request for a maximum of 5 times
+        for attempt in range(5):
             if repo.merge_with_rebase_if_possible("reitzensteinm/duopoly", pr_id):
                 print(f"Merged PR: {pr_id}")
-                break  # Break out of the retry loop
+                break
             else:
                 print(f"Attempt {attempt + 1}: Could not merge PR: {pr_id}")
-                if attempt < 4:  # If this wasn't the last attempt
-                    time.sleep(
-                        5
-                    )  # Sleep for 5 seconds before trying again, if this wasn't the last attempt
+                if attempt < 4:
+                    time.sleep(5)
                 else:
-                    print(
-                        f"Failed to merge PR {pr_id} after 5 attempts."
-                    )  # If we've used all our retries
+                    print(f"Failed to merge PR {pr_id} after 5 attempts.")
     repo.fetch_new_changes()
 
 
 def replace_function_in_code(source_code, function_name, new_function_code):
-    # Parsing the source code
     tree = ast.parse(source_code)
-
-    # Locating the old function and replacing it with the new function
     for node in ast.walk(tree):
         if isinstance(node, ast.FunctionDef) and node.name == function_name:
             new_function_node = ast.parse(new_function_code)
@@ -59,32 +48,31 @@ def main(dry_run=False, issue_name=None) -> None:
     def process_open_issue(issue):
         if issue_name is None or issue_name in issue.title:
             for attempt in range(MAX_RETRIES):
-                trace = create_trace(f"Issue {issue.id}")
-                bind_trace(trace)
+                trace_instance = create_trace(f"{issue.title}")
+                bind_trace(trace_instance)
                 try:
                     process_issue(issue, dry_run)
-                    break  # Breaking out of the loop if the process_issue is successful
+                    break
                 except Exception as e:
                     cprint(
-                        f"Attempt {attempt + 1} failed for issue {issue.id} with error: {str(e)}\n{traceback.format_exc()}",
+                        f"""Attempt {attempt + 1} failed for issue {issue.title} with error: {str(e)}
+{traceback.format_exc()}""",
                         "red",
                     )
-                    trace.add_trace_data(EXCEPTION, str(e))
+                    trace(EXCEPTION, str(e))
                     if attempt == MAX_RETRIES - 1:
                         print(
-                            f"Failed to process issue {issue.id} after {MAX_RETRIES} attempts."
+                            f"Failed to process issue {issue.title} after {MAX_RETRIES} attempts."
                         )
                         raise
                     else:
-                        print(f"Retrying to process issue {issue.id}...")
+                        print(f"Retrying to process issue {issue.title}...")
 
     if dry_run:
         process_open_issue(open_issues[0])
         return
-
     with ThreadPoolExecutor(max_workers=5) as executor:
         results = executor.map(process_open_issue, open_issues)
-
     for result in results:
         pass
 
