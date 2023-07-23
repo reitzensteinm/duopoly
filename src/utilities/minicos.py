6b807e7b9f425a029cbb1fc634d9c0e5ab7022ae
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-23 20:49:48 +1000

    Add search to VectorStore
    Prompt: "Depends on #676 
    
    Add a search function to the VectorStore class. It should take a numpy vec, and an integer that represents the number of items to return.
    
    The function should return the values of the n entries where the keys have the highest cosine similarity with the numpy vec passed into the function.
    
    The return type should be a list of strings."

--- src/utilities/minicos.py
+++ src/utilities/minicos.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 
 
@@ -19,3 +20,11 @@ class VectorStore:
 
     def add_entry(self, key: np.ndarray, value: str):
         self.entries.append(Entry(key, value))
+
+    def search(self, vec: np.ndarray, n: int) -> List[str]:
+        # Calculate similarities and track top n
+        similarities = [
+            (entry.value, cosine_similarity(vec, entry.key)) for entry in self.entries
+        ]
+        similarities.sort(key=lambda x: x[1], reverse=True)
+        return [similarity[0] for similarity in similarities[:n]]
