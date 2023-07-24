ea4829c4efbbcec68071664cebbf313ca29aef30
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-25 00:02:31 +1000

    Add run_eval function
    Prompt: "In evals.py create a run_eval function that takes an Eval class, as well as a directory. 
    
    Move the logic inside the loop in proces_evals function to this new function, and update the loop to call it."

--- src/evals/evals.py
+++ src/evals/evals.py
@@ -10,14 +10,17 @@ class Eval:
     tests: List[str]
 
 
+def run_eval(eval: Eval, directory: str):
+    test_names = " ".join(eval.tests)
+    result = run_pytest(f"{directory}/src", test_names)
+    if result is not None:
+        raise Exception(f"Pytest failed for tests: {test_names}\n{result}")
+
+
 def process_evals(directory: str):
     with open(f"{directory}/prompts.yaml", "r") as file:
         data = yaml.safe_load(file)
         evals = [Eval(prompt=key, tests=value["tests"]) for key, value in data.items()]
 
     for eval in evals:
-        test_names = " ".join(eval.tests)
-        result = run_pytest(f"{directory}/src", test_names)
-
-        if result is not None:
-            raise Exception(f"Pytest failed for tests: {test_names}\n{result}")
+        run_eval(eval, directory)
