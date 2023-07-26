b476f44154e83e69ded5b42a702fd07aa8c5b6a8
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-26 23:28:22 +1000

    Fix advice generation
    Prompt: "In generate_advice, the goal dict should be passed into the load_prompt function to render the prompt. The result of the render should be passed into gpt_query as message."

--- src/tools/advice.py
+++ src/tools/advice.py
@@ -4,10 +4,9 @@ from gpt import gpt_query
 
 
 def generate_advice(goal: str) -> str:
-    prompt = load_prompt("advice.txt")
-    goal_dict = {"goal": goal}
+    prompt = load_prompt("advice.txt", {"goal": goal})
     response = gpt_query(
-        message=goal_dict,
+        message=prompt,
         system="Given the stated goal, return a list of the advice that is relevant",
     )
     return response
