c8a30c4706b8e63999b696e0f54693127f7f1540
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-27 00:00:23 +1000

    Fix advice path
    Prompt: "In generate_advice, load_prompt paths shouldn't contain a trailing .txt"

--- src/tools/advice.py
+++ src/tools/advice.py
@@ -4,7 +4,7 @@ from gpt import gpt_query
 
 
 def generate_advice(goal: str) -> str:
-    prompt = load_prompt("advice.txt", {"goal": goal})
+    prompt = load_prompt("advice", {"goal": goal})
     response = gpt_query(
         message=prompt,
         system="Given the stated goal, return a list of the advice that is relevant",
