5adb9fb75414bb65b727d7506f8cfda04e66f805
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-26 23:29:07 +1000

    Modify Summarize Format
    Prompt: "Modify the SYSTEM_SUMMARIZE prompt in these ways:
    
    1) Change CONSTANT to CONSTANT_NAME
    2) Above each class, function, constant line, add a comment that reads something like:
    
    # One line comment that describes the purpose of this <function|constant|class>"

--- src/tools/summarize.py
+++ src/tools/summarize.py
@@ -3,12 +3,15 @@ from gpt import cached_gpt_query, GPT_4
 SYSTEM_SUMMARIZE = """
 Summarize the following source code, extracting classes, functions and constants by extracting the definitions into the following format:
 
+# One line comment that describes the purpose of this class
 class Test
    fn class_method(self)
 
+# One line comment that describes the purpose of this function
 fn a_function(arg1,arg2)
 
-CONSTANT
+# One line comment that describes the purpose of this constant
+CONSTANT_NAME
 """
 
 
