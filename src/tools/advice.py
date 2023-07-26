bcbe41de0302043e3e6210ab6e87f434ff6f9ff1
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-07-27 02:17:34 +1000

    Update advice system message
    Prompt: "In advice.py, update the system message string by adding this line at the end:
    
    "Do not add new advice. Just return a list of the supplied advice verbatim that is relevant to the stated goal.""

--- src/tools/advice.py
+++ src/tools/advice.py
@@ -7,6 +7,6 @@ def generate_advice(goal: str) -> str:
     prompt = load_prompt("advice", {"goal": goal})
     response = gpt_query(
         message=prompt,
-        system="Given the stated goal, return a list of the advice that is relevant",
+        system="Given the stated goal, return a list of the advice that is relevant. Do not add new advice. Just return a list of the supplied advice verbatim that is relevant to the stated goal.",
     )
     return response
