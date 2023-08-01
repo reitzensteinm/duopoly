c2e2ea56054a1e71608fa3aece8bdb028a4034ed
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-08-01 23:23:05 +1000

    Load prompts
    Prompt: "In replace_file.py, load SYSTEM_REPLACE_THINK and SYSTEM_REPLACE from replace_think.txt and replace.txt respectively, using load_prompt. Remove the extension when calling load_prompt."

--- src/tools/replace_file.py
+++ src/tools/replace_file.py
@@ -1,34 +1,9 @@
 from gpt import gpt_query
+from utilities.prompts import load_prompt
 
-SYSTEM_REPLACE_THINK = """
-You are a helpful programming assistant. You will be given a file as well as instructions to modify it.
+SYSTEM_REPLACE_THINK = load_prompt("replace_think")
 
-Plan out step-by-step how you'd like to make the change, but don't start writing code yet. 
-You will be asked to write code in the next step.
-
-Requirements:
-"Do not include instructions to modify anything but the target file"
-
-An example plan might be:
-
-1) Modify function count_functions to also count classes
-2) Throw an exception when an unterminated string is passed to parse_file
-"""
-
-SYSTEM_REPLACE = """
-You are a helpful programming assistant. You will be given a file as well as instructions to modify it. 
-
-Requirements:
-1) Please make ONLY the changes requested.
-2) Reply only with the updated file. Do not include any commentary. 
-3) Do not add markdown quotes around code in your responses. 
-4) Use tabs for indentation.
-5) Do not add any tests unless specifically requested.
-6) Return the entire file without skipping anything or stopping before the end, no matter how long it is. 
-7) Do not include line numbers in your output.
-8) Include content from only the file specified.
-9) Do not include the file name or any other content that should not be included in the file itself.
-"""
+SYSTEM_REPLACE = load_prompt("replace")
 
 
 def modify_file(original_file, instructions, context="", file_name=None):
