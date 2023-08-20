9e2725f8ab4dee8614d05be9ffd265d4e2b28b8f
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-08-21 00:34:07 +1000

    Move Test Index
    Prompt: "Move test_index.py to be next to the file it is testing. In addition, remove imports from the file - they're not necessary as the code is strings."

--- src/tools/test_index.py
+++ src/tools/test_index.py
@@ -1,13 +1,11 @@
 from tools import index
-import django
-import numpy
 
 django_query = """
 # Django queryset search example
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField(max_length=30)
+	name = models.CharField(max_length=30)
 
 p = Person(name="John Doe")
 p.save()
@@ -19,13 +17,13 @@ people = Person.objects.filter(name="John Doe")
 quicksort_code = """
 # Quicksort example
 def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+	if len(arr) <= 1:
+		return arr
+	pivot = arr[len(arr) // 2]
+	left = [x for x in arr if x < pivot]
+	middle = [x for x in arr if x == pivot]
+	right = [x for x in arr if x > pivot]
+	return quicksort(left) + middle + quicksort(right)
 
 print(quicksort([3,6,8,10,1,2,1]))
 """
