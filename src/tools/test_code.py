73dbb2e35bd5b90b56bc76fba60d36d41b035488
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-09-10 22:03:05 +1000

    Add test for replacing constant
    Prompt: "In test_code.py, add a test that tests a constant being replaced. 
    
    Rename ORIGINAL_FUNCTION to ORIGINAL_CODE, and add a constant in that code at the top level. Then replace it within the test function, as a separate test.
    
    Mark the test as skipped, though - it doesn't yet exist."

--- src/tools/test_code.py
+++ src/tools/test_code.py
@@ -1,22 +1,36 @@
 import pytest
 from tools.code import replace_node
 
-ORIGINAL_FUNCTION = """
+ORIGINAL_CODE = """
 def greet():
 	print("Hello, world!")
+CONSTANT = 'old_value'
 """
 
-NEW_FUNCTION = """
+NEW_CODE = """
 def greet():
 	print("Hello, everyone!")
 """
+
+NEW_CONSTANT = "CONSTANT = 'new_value'"
 
 
 def test_replace_node():
     # Use the replace_node function to replace the original function
     # with the new function
-    result = replace_node(ORIGINAL_FUNCTION, "greet", NEW_FUNCTION)
+    result = replace_node(ORIGINAL_CODE, "greet", NEW_CODE)
 
     # Check if NEW_FUNCTION is in the result and ORIGINAL_FUNCTION is not
     assert "Hello, everyone!" in result
     assert "Hello, world!" not in result
+
+
+@pytest.mark.skip
+def test_replace_constant():
+    # Use the replace_node function to replace the CONSTANT
+    # with the NEW_CONSTANT
+    result = replace_node(ORIGINAL_CODE, "CONSTANT", NEW_CONSTANT)
+
+    # Check if NEW_CONSTANT is in the result and CONSTANT is not
+    assert "CONSTANT = 'new_value'" in result
+    assert "CONSTANT = 'old_value'" not in result
