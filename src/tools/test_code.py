7915497533f56d979bc870878762dc422b2ae6f7
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-09-11 23:47:55 +1000

    Add dataclass test to test_code.py
    Prompt: "Add a dataclass to ORIGINAL_CODE in test_code.py
    
    Add a test that tests replacing this dataclass. But mark it as skipped for now. It doesn't work!"

--- src/tools/test_code.py
+++ src/tools/test_code.py
@@ -1,15 +1,29 @@
 import pytest
 from tools.code import replace_node
+from dataclasses import dataclass
 
 ORIGINAL_CODE = """
 def greet():
 	print("Hello, world!")
 CONSTANT = 'old_value'
+
+@dataclass
+class Data:
+	name: str
+	age: int
 """
 
 NEW_CODE = """
 def greet():
 	print("Hello, everyone!")
+"""
+
+NEW_DATA_CLASS = """
+@dataclass
+class Data:
+	name: str
+	age: int
+	country: str
 """
 
 NEW_CONSTANT = "CONSTANT = 'new_value'"
@@ -23,6 +37,17 @@ def test_replace_node():
     # Check if NEW_FUNCTION is in the result and ORIGINAL_FUNCTION is not
     assert "Hello, everyone!" in result
     assert "Hello, world!" not in result
+
+
+@pytest.mark.skip(reason="Dataclass replacement test doesn't functioning properly yet.")
+def test_replace_dataclass():
+    # Use the replace_node function to replace the Dataclass
+    # with the NEW_DATA_CLASS
+    result = replace_node(ORIGINAL_CODE, "Data", NEW_DATA_CLASS)
+
+    # Check if NEW_DATA_CLASS is in the result and original dataclass is not
+    assert "@dataclasses.dataclass name" not in result
+    assert "country: str" in result
 
 
 def test_replace_constant():
