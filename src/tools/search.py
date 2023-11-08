8db411ad5945c35eb5f0cabacd87ff52e6f7caa9
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-11-08 13:14:35 +1100

    Make search case insensitive
    Prompt: "Make the search tool case insensitive."

--- src/tools/search.py
+++ src/tools/search.py
@@ -1,5 +1,8 @@
 def search_tool(files: dict, search_string: str) -> list:
+    search_string = search_string.lower()
     matching_files = [
-        filename for filename, content in files.items() if search_string in content
+        filename
+        for filename, content in files.items()
+        if search_string in content.lower()
     ]
     return matching_files
