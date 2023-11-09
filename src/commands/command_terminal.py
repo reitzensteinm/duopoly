401af46ffa050345b4c90a97fd7b8d7ea4aa0c7a
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-11-09 11:56:37 +1100

    Add terminal command
    Prompt: "Title: Add terminal command
    Description: Add a new Command called command_terminal.py, which runs a given string on the command line. Use command_search.py as an example. Add the new command to commands.py."

---
+++ src/commands/command_terminal.py
@@ -0,0 +1,59 @@
+import subprocess
+from .command import Command
+from .state import State
+
+
+class Terminal(Command):
+    """
+    Class representing a Terminal command.
+    """
+
+    @classmethod
+    def name(cls) -> str:
+        return "Terminal"
+
+    @property
+    def terminal(self):
+        return True
+
+    def __init__(self, command_string: str):
+        self.command_string: str = command_string
+
+    @staticmethod
+    def schema() -> dict:
+        """
+        Returns the schema for the Terminal command.
+        """
+        return {
+            "name": "Terminal",
+            "description": "Execute a given string as a terminal command",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "command_string": {
+                        "type": "string",
+                        "description": "The terminal command to execute",
+                    }
+                },
+                "required": ["command_string"],
+            },
+        }
+
+    @staticmethod
+    def load_from_json(json_data: dict) -> "Terminal":
+        """
+        Loads the Terminal command from the provided json_data.
+        """
+        return Terminal(json_data["command_string"])
+
+    def execute(self, state: State) -> str:
+        """
+        Executes the Terminal command.
+        """
+        result = subprocess.run(
+            self.command_string, shell=True, capture_output=True, text=True
+        )
+        return result.stdout
+
+    def __str__(self):
+        return f"Function Called: Terminal command_string={self.command_string}"
