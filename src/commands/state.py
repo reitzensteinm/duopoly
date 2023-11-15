f5ef3c4ff8ddc2a956815a695d5ff51997d07aa4
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-11-15 13:30:06 +0800

    Add information dict to State
    Prompt: "Title: Add information dict to State
    Description: Add an information dict to State, which has keys/values of strings.
    
    Add a function to render the dict, by looping over the k/v pairs and rendering:
    
    key1:
    value1
    
    key2:
    value2 
    
    
    etc
    
    In command_loop_iterate, render the dict and pass it into gpt_query after scratch."

--- src/commands/state.py
+++ src/commands/state.py
@@ -9,3 +9,10 @@ class State:
         self.scratch: str = ""
         self.target_dir: str = target_dir
         self.last_command = None
+        self.information: Dict[str, str] = {}
+
+    def render_information(self) -> str:
+        rendered_info = []
+        for key, value in self.information.items():
+            rendered_info.append(f"{key}:\n{value}")
+        return "\n\n".join(rendered_info)
