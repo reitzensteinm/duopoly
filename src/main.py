a4e26bb56dbb57837f03e26d4a8bf3ccd7e8acb6
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-11-22 14:10:18 +1100

    Move max workers to Settings
    Prompt: "Title: Move max workers to Settings
    Description: Move the max_workers property in main.py to settings.py as a global file constant, MAX_WORKERS. Reference this variable instead of it being hardcoded. Make it 10."

--- src/main.py
+++ src/main.py
@@ -87,7 +87,7 @@ def process_repository(dry_run=False, issue_name=None, repository="") -> None:
     if dry_run:
         process_open_issue(open_issues[0])
         return
-    with ThreadPoolExecutor(max_workers=5) as executor:
+    with ThreadPoolExecutor(max_workers=settings.MAX_WORKERS) as executor:
         results = executor.map(process_open_issue, open_issues)
     for result in results:
         pass
