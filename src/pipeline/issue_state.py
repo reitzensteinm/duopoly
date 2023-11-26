bbd2379b74b191411a1fcea48f3ed199f8fef9eb
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-11-26 13:41:11 +1100

    Fix IssueState
    Prompt: "Title: Fix IssueState
    Description: In IssueState, remove the __dict__ custom method, and also remove the ".json" extension from the specified key."

--- src/pipeline/issue_state.py
+++ src/pipeline/issue_state.py
@@ -5,7 +5,10 @@ class IssueState:
     """Represents the state of an issue with a retry count.
 
     Args:
-            id (int): The identifier of the issue.
+        id (int): The identifier of the issue.
+
+    Returns:
+        IssueState: An instance representing the issue state.
 
     """
 
@@ -13,7 +16,7 @@ class IssueState:
         """Initialize the IssueState with an ID and a retry count set to zero.
 
         Args:
-                id (int): The identifier of the issue.
+            id (int): The identifier of the issue.
 
         """
         self.id = id
@@ -21,21 +24,38 @@ class IssueState:
 
     @staticmethod
     def retrieve_by_id(id: int) -> "IssueState":
+        """Retrieve the issue state by id, if not found, create a new one and store.
+
+        Args:
+            id (int): The identifier of the issue.
+
+        Returns:
+            IssueState: The retrieved or newly created issue state.
+
+        """
         try:
-            issue_dict = read(f"issue-{id}.json")
+            issue_dict = read(f"issue-{id}")
             new_issue = IssueState(id)
             new_issue.__dict__.update(issue_dict)
             return new_issue
         except FileNotFoundError:
             new_issue = IssueState(id)
-            write(f"issue-{id}.json", new_issue)
+            write(f"issue-{id}", new_issue)
             return new_issue
 
     def store(self) -> None:
-        write(f"issue-{self.id}.json", self)
+        """Store the current issue state.
 
-    def __dict__(self) -> dict:
-        return self.__dict__.copy()
+        Returns:
+            None
+        """
+        write(f"issue-{self.id}", self)
 
     def __str__(self) -> str:
+        """Return a string representation of the issue state.
+
+        Returns:
+            str: The string representation of the issue state.
+
+        """
         return str(self.__dict__)
