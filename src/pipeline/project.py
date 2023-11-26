d9adf73cfa7b765ea095192436e62cef5639a349
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-11-26 16:27:47 +1100

    Add code_paths to Project
    Prompt: "Title: Add code_paths to Project
    Description: Add code_paths to the Project class, which is a list of strings."

--- src/pipeline/project.py
+++ src/pipeline/project.py
@@ -1,3 +1,9 @@
 class Project:
     def __init__(self, path: str):
+        """Initialize the Project with a path and an empty list of code paths.
+
+        Args:
+                path: A string representing the path to the project.
+        """
         self.path = path
+        self.code_paths: List[str] = []
