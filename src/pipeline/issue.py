4e930053d27ffd76d30e59f8566b9251aa7ef365
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-11-26 14:35:38 +1100

    Fix retries bug
    Prompt: "Title: Fix retries bug
    Description: In the process_issue function, max_issue_retries should be accessed via get_settings().max_issue_retries
    
    Leave PYLINT_RETRIES as is."

--- src/pipeline/issue.py
+++ src/pipeline/issue.py
@@ -147,16 +147,16 @@ def prepare_branch(issue: Issue, dry_run: bool) -> str:
 def process_issue(issue: Issue, dry_run: bool) -> None:
     """Processes a single issue by setting up a branch, applying prompts, running checks, and creating pull requests.
 
-    Checks the retry count before processing and skips the issue with a message if it exceeds max_issue_retries.
+    Checks the retry count before processing and skips the issue with a message if it exceeds max_issue_retries obtained from get_settings().
     Increases the retry count after an open PR check if no open PR was found, and does not process the issue if the author is not marked as an admin,
     if the issue is not open, or if there is already an open PR for the issue.
 
     Params:
-            issue (Issue): The issue to be processed.
-            dry_run (bool): If true, does not conduct any writes or branch modifications.
+                    issue (Issue): The issue to be processed.
+                    dry_run (bool): If true, does not conduct any writes or branch modifications.
 
     Returns:
-            None
+                    None
     """
     if issue.author not in settings.ADMIN_USERS:
         return
@@ -167,7 +167,8 @@ def process_issue(issue: Issue, dry_run: bool) -> None:
     ):
         return
     issue_state = IssueState.retrieve_by_id(issue.id)
-    if issue_state.retry_count > settings.max_issue_retries:
+    settings_instance = settings.get_settings()
+    if issue_state.retry_count > settings_instance.max_issue_retries:
         print(
             f"\x1b[91mSkipping processing of issue {issue.number} due to too many attempts\x1b[0m"
         )
