2e4d044b050eff19d0b3699b33f34b65c3a2da7a
Duopoly <133977416+duopoly@users.noreply.github.com>
2023-11-29 00:20:17 +1100

    Clean up Hydrate
    Prompt: In hydrate.py, remove unused imports.
    
    Depends on #1336

--- src/tools/hydrate.py
+++ src/tools/hydrate.py
@@ -1,9 +1,16 @@
-import os
 from utilities.prompts import load_prompt
 from gpt import gpt_query
 
 
 def hydrate_code(source_code: str) -> str:
+    """Hydrates the provided source code using AI-driven suggestions.
+
+    Args:
+    source_code: The code to be hydrated, as a string.
+
+    Returns:
+    The hydrated source code, as a string.
+    """
     prompt = load_prompt("hydrate")
     response = gpt_query(message=source_code, system=prompt)
     return response
