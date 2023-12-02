a53f8941d0053b82b72e603c9fa9c71ec504220b
reitzensteinm <133977416+duopoly@users.noreply.github.com>
2023-12-02 19:02:33 +1100

    Trace pylint output with the system tag
    Prompt: "Title: Trace pylint output with the system tag
    Description: None"

--- src/tools/pylint.py
+++ src/tools/pylint.py
@@ -1,5 +1,7 @@
 import subprocess
 from typing import Optional
+from tracing.trace import create_trace, bind_trace, trace
+from tracing.tags import SYSTEM
 
 
 def run_pylint(directory: str, warnings: bool = False) -> Optional[str]:
@@ -7,6 +9,10 @@ def run_pylint(directory: str, warnings: bool = False) -> Optional[str]:
 
     This function takes a 'directory' parameter which is a string specifying the directory to be analyzed. The 'warnings' parameter, when set to True, includes warnings in the results. Returns a string containing the lint results with the directory replaced by 'src', or None if there are no lint issues.
     """
+    trace_name: str = "pylint_trace"
+    pylint_trace = create_trace(trace_name)
+    bind_trace(pylint_trace)
+
     if warnings:
         command = ["pylint", "--disable=R,C", directory]
     else:
@@ -18,6 +24,9 @@ def run_pylint(directory: str, warnings: bool = False) -> Optional[str]:
         text=True,
     )
     if proc.returncode != 0:
-        return proc.stdout.replace(directory, "src")
+        output: str = proc.stdout.replace(directory, "src")
+        trace(SYSTEM, f"Pylint executed with output: {output}")
+        return output
     else:
+        trace(SYSTEM, f"Pylint executed with no issues found in {directory}")
         return None
